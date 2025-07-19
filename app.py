8828cdf42e611a47b82d22434be3d6ff14272c5c
JeongWon4034 <leewon1238@kyonggi.ac.kr>
2025-07-19 21:46:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -40,9 +40,9 @@ def format_cafes(cafes_df):
     elif len(cafes_df) == 1:
         row = cafes_df.iloc[0]
         if all(x not in row["c_review"] for x in ["없음", "없읍"]):
-            return f"☕ **주변 추천 카페**\n\n- **{row['c_name']}** (⭐ {row['c_value']})  \n\"{row['c_review']}\""
+            return f" **{row['c_name']}** (⭐ {row['c_value']})  \n\"{row['c_review']}\""
         else:
-            return f"☕ **주변 추천 카페**\n\n- **{row['c_name']}** (⭐ {row['c_value']})"
+            return f"**{row['c_name']}** (⭐ {row['c_value']})"
 
     else:
         grouped = cafes_df.groupby(['c_name', 'c_value'])
